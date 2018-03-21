fdaffae0af289c0db8dc90c1a28dda34ad4140e9
Rekha projects <l.s.rekha.29@gmail.com>
2018-03-21 11:57:18 +0530

    rekha commit

--- app/src/main/java/com/example/mansopresk21/mansoshopping/SplashActivity.java
+++ app/src/main/java/com/example/mansopresk21/mansoshopping/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
-
+        //setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable()
         {
             @Override

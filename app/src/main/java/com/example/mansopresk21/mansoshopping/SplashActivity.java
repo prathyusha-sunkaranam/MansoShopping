885aedd9cd5bb264a24e64648081ee6c0b7bf04b
prathyusha-sunkaranam <prathyu565@gmail.com>
2018-03-23 11:49:50 +0530

    fragmentback commit

--- app/src/main/java/com/example/mansopresk21/mansoshopping/SplashActivity.java
+++ app/src/main/java/com/example/mansopresk21/mansoshopping/SplashActivity.java
@@ -8,7 +8,7 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 2500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
